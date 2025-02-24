be229b7192a10abff4ebf67b9a36713c71c0b5f6
Pratik Saha <pratik.saha14@gmail.com>
2025-02-24 10:55:28 +0530

    default error data made null

--- src/middleware/is-auth.ts
+++ src/middleware/is-auth.ts
@@ -7,14 +7,14 @@ export default async (req : any, res : Response, next: NextFunction) => {
         let token = "";
         if(!req.get('Authorization')){
             const error = new Error("You are not authorized");
-            const cerror = new CustomError({statusCode: 403, error, data: []});
+            const cerror = new CustomError({statusCode: 403, error, data: null});
             throw cerror;
         }
         token = req.get('Authorization')!.split(' ')[1];
         const data : any = jwt.verify(token, 'areallylongstringaskey');         
         if(!data) {
             const error = new Error("Not authenticated");
-            const cerror = new CustomError({statusCode: 401, error, data: []});
+            const cerror = new CustomError({statusCode: 401, error, data: null});
             throw cerror;
         }
 
@@ -23,7 +23,7 @@ export default async (req : any, res : Response, next: NextFunction) => {
     } catch(err : any) {
         if(!err.statusCode) {
             const error = new Error("Try login again");
-            err = new CustomError({statusCode: 403, error, data: []});
+            err = new CustomError({statusCode: 403, error, data: null});
         }
         next(err)
     }
